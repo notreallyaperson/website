5ffb0dde248d6eae8ba26deeb4fe6dbbb5ebfa69
notreallyaperson <xavinash931103@gmail.com>
2020-09-02 01:09:07 +0800

    Updates

--- precache-manifest.26ccebdc3868b21157e7b518a6bc3b62.js
+++ precache-manifest.26ccebdc3868b21157e7b518a6bc3b62.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "283ed4eeaf3833fa8ccea4a2d3686414",
+    "revision": "b00f4215bc5c7f914553ad570acab80b",
     "url": "/website/index.html"
   },
   {
-    "revision": "66f2b37a4c61bb74d905",
+    "revision": "1016317047a0e77b28a8",
     "url": "/website/static/css/main.f7b845ed.chunk.css"
   },
   {
-    "revision": "86e22377dce69f3a51fa",
-    "url": "/website/static/js/2.d23840d2.chunk.js"
+    "revision": "cc05f363b0800ed4925f",
+    "url": "/website/static/js/2.0cfa6f21.chunk.js"
   },
   {
     "revision": "161c6b8d7fe9f3d9d1c896db3e01ea03",
-    "url": "/website/static/js/2.d23840d2.chunk.js.LICENSE.txt"
+    "url": "/website/static/js/2.0cfa6f21.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "66f2b37a4c61bb74d905",
-    "url": "/website/static/js/main.c93248f5.chunk.js"
+    "revision": "1016317047a0e77b28a8",
+    "url": "/website/static/js/main.eab6c7c1.chunk.js"
   },
   {
     "revision": "30f942bb7783c4b076a0017ec0876fab",
-    "url": "/website/static/js/main.c93248f5.chunk.js.LICENSE.txt"
+    "url": "/website/static/js/main.eab6c7c1.chunk.js.LICENSE.txt"
   },
   {
     "revision": "1b767ea0529ce0b18cc1",
@@ -36,6 +36,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/website/static/media/Background-image.bf3207e0.jpg"
   },
   {
+    "revision": "dcc11f0795e67dc2a5ae1d28ff5b4898",
+    "url": "/website/static/media/Jack_Profile.dcc11f07.jpg"
+  },
+  {
     "revision": "581a4d38735167782a8d36691ee4fd63",
     "url": "/website/static/media/Qworky_Logo.581a4d38.png"
   },
@@ -44,56 +48,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/website/static/media/Qworky_Logo_Inverted.d30d946d.png"
   },
   {
-    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
-    "url": "/website/static/media/antoine-barres.dfbf459c.jpg"
-  },
-  {
-    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
-    "url": "/website/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
-  },
-  {
-    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
-    "url": "/website/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
-  },
-  {
-    "revision": "61c4e0007254eb55e4691291881a4bfb",
-    "url": "/website/static/media/clouds.61c4e000.png"
-  },
-  {
-    "revision": "df0cdbfb2c70d9857e3776eb83a5a4eb",
-    "url": "/website/static/media/creative-tim-white-slim2.df0cdbfb.png"
-  },
-  {
-    "revision": "2051e0e847124c536df9c01a3f672032",
-    "url": "/website/static/media/erik-lucatero-2.2051e0e8.jpg"
-  },
-  {
-    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
-    "url": "/website/static/media/fabio-mangione.f26c34bb.jpg"
-  },
-  {
     "revision": "1331ba97560cc13103d379a8843ad1dc",
     "url": "/website/static/media/federico-beccari.1331ba97.jpg"
-  },
-  {
-    "revision": "7812184c3710d9623c684956fe048ddd",
-    "url": "/website/static/media/fog-low.7812184c.png"
   },
   {
     "revision": "efe053682ec97d330adad93c55dc7bd0",
     "url": "/website/static/media/ilya-yakover.efe05368.jpg"
   },
   {
-    "revision": "76ff47ae5c64ccda5af18326bb272058",
-    "url": "/website/static/media/joe-gardner-2.76ff47ae.jpg"
-  },
-  {
     "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
     "url": "/website/static/media/joshua-stannard.8a26aabc.jpg"
-  },
-  {
-    "revision": "e06d84cb846db2a463ac3d904ac0c362",
-    "url": "/website/static/media/kaci-baum-2.e06d84cb.jpg"
   },
   {
     "revision": "d65b63163c9caec8e994304bf1b1cfcc",
@@ -126,9 +90,5 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "29df8117c07250e9b1ea8e3a67627f67",
     "url": "/website/static/media/soroush-karimi.29df8117.jpg"
-  },
-  {
-    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
-    "url": "/website/static/media/uriel-soberanes.fe3a7838.jpg"
   }
 ]);
