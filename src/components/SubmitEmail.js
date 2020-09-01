a2f6469fd358653a7bc226842833b523061a4365
notreallyaperson <xavinash931103@gmail.com>
2020-09-01 10:19:17 +0800

    first commit

---
+++ src/components/SubmitEmail.js
@@ -0,0 +1,175 @@
+import React, { useState } from "react";
+
+// reactstrap components
+import {
+  Button,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+  Alert,
+} from "reactstrap";
+import axios from 'axios';
+
+function SubmitEmail() {
+
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [message, setMessage] = useState('')
+    const [errorMessages, setErrorMessages] = useState([])
+    const [alertDanger, setAlertDanger] = useState([]);
+    const [alertSuccess, setAlertSuccess] = React.useState(false);
+    const [loading, setLoading] = useState(false)
+
+    const submitMessage = async e => {
+        e.preventDefault()
+        setLoading(true);
+        var arr = []
+        if(!name){arr.push('Name is Required')}
+        if(!RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test(email))
+        {arr.push('Valid Email is Required')}
+        if(!message){arr.push('Message is Required')}
+        setErrorMessages(arr)
+        if(arr.length>0){
+            var notificationState = arr.map(() => true)
+            setLoading(false);
+            return setAlertDanger(notificationState)
+        }
+        const request = {
+            PassCode: "ThisisaPostcallsonobodyshouldseethisbuteventuallyweneedaproperuniquecode",
+            name,
+            email,
+            message
+        }
+        
+        const URI = "https://script.google.com/macros/s/AKfycbzqQplcfy92HR0isYDhcmwmBaXSj6Lp3M33OyPl7AHkZxCwRGI/exec"
+        var reqBody = JSON.stringify(request)
+        const res = await axios.post(URI, reqBody);
+
+        if (res.data === "Success") {
+            setAlertSuccess(true)
+            setLoading(false);
+            setEmail('')
+            setName('')
+            setMessage('')
+        } else {
+            setErrorMessages([res.data])
+            setAlertDanger([false])
+        }
+    }
+
+    const closeNotification = i => {
+        var arr = alertDanger.map((e,j) => {
+            if(i===j){return false}
+            return e
+        })
+        setAlertDanger(arr)
+    }
+
+    document.documentElement.classList.remove("nav-open");
+    React.useEffect(() => {
+      document.body.classList.add("profile-page");
+      return function cleanup() {
+        document.body.classList.remove("profile-page");
+      };
+    });
+
+    return (
+        <Container>
+            <Alert color="success" isOpen={alertSuccess}>
+                <Container>
+                    <button
+                    type="button"
+                    className="close"
+                    data-dismiss="alert"
+                    aria-label="Close"
+                    onClick={() => setAlertSuccess(false)}
+                    >
+                    <i style={{color: '#061708'}} className="nc-icon nc-simple-remove" />
+                    </button>
+                    <span style={{color: '#061708'}}>We have recieved your message and will get back to you.</span>
+                </Container>
+            </Alert>
+            {errorMessages.map((e,i) => <Alert key ={e} className="alert-with-icon" color="danger" isOpen={alertDanger[i]}>
+                    <Container>
+                        <div className="alert-wrapper">
+                        <button
+                            type="button"
+                            className="close"
+                            data-dismiss="alert"
+                            aria-label="Close"
+                            onClick={() => closeNotification(i)}
+                        >
+                            <i className="nc-icon nc-simple-remove" />
+                        </button>
+                        <div className="message">
+                            <i className="nc-icon nc-bell-55" /> {e}
+                        </div>
+                        </div>
+                    </Container>
+                </Alert>
+            )}
+            <Row>
+              <Col className="ml-auto mr-auto" md="8">
+                <h2 className="text-center">Keep in touch?</h2>
+                <Form className="contact-form">
+                  <Row>
+                    <Col md="6">
+                      <label>Name</label>
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="nc-icon nc-single-02" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input placeholder="Name" type="text" value={name} onChange={e => setName(e.target.value)}/>
+                      </InputGroup>
+                    </Col>
+                    <Col md="6">
+                      <label>Email</label>
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="nc-icon nc-email-85" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input 
+                            placeholder="Email" 
+                            type="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                      </InputGroup>
+                    </Col>
+                  </Row>
+                  <label>Message</label>
+                  <Input
+                    placeholder="Let us know how we can help..."
+                    type="textarea"
+                    rows="4"
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
+                  />
+                  <Row>
+                    <Col className="ml-auto mr-auto" md="4">
+                      {loading?
+                      <Button className="btn-fill" color="danger" size="lg" disabled>
+                          Send Message
+                      </Button>
+                      :<Button className="btn-fill" color="danger" size="lg" onClick={e => submitMessage(e)}>
+                        Send Message
+                      </Button>}
+                    </Col>
+                  </Row>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
+    )
+}
+
+export default SubmitEmail;
---
+++ src/views/examples/LandingPage.js
@@ -0,0 +1,330 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import LandingPageHeader from "components/Headers/LandingPageHeader.js";
+import SubmitEmail from "components/SubmitEmail";
+
+// Icon Imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers, faClock, faCogs, faChartLine } from '@fortawesome/free-solid-svg-icons';
+
+function LandingPage() {
+  document.documentElement.classList.remove("nav-open");
+  React.useEffect(() => {
+    document.body.classList.add("profile-page");
+    return function cleanup() {
+      document.body.classList.remove("profile-page");
+    };
+  });
+  return (
+    <>
+      <ExamplesNavbar />
+      <LandingPageHeader />
+      <div className="main">
+        <div className="section text-center">
+          <Container>
+            <Row>
+              <Col className="ml-auto mr-auto" md="8">
+                <h2 className="title">Our History</h2>
+                <h5 className="description">
+                Qworky is a ERP solutions provider targeting SMEs in Malaysia
+                <br />
+                <br />
+                From our debut in 2019, Qworky got involved in multiple projects, allowing the company to grow steadily. 
+                Malaysia has always been lacking in terms of digital infrastructure. 
+                So Qworky was formed to fill that gap
+                <br />
+                <br />
+                We strive to make digitalisation affordable and accessible.
+                <br />
+                We strive to create scalable personalized solutions
+                </h5>
+                <br />
+                {/* <Button
+                  className="btn-round"
+                  color="info"
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  See Details
+                </Button> */}
+              </Col>
+            </Row>
+            <br />
+            <br />
+            <Row>
+              <Col md="3">
+                <div className="info">
+                  <div className="icon icon-info">
+                    <FontAwesomeIcon icon={faUsers} />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">Task Management</h4>
+                    <p className="description">
+                      Manage all projects and track staff perfomance and output in a centralized location
+                    </p>
+                    {/* <Button className="btn-link" color="info" href="#pablo">
+                      See more
+                    </Button> */}
+                  </div>
+                </div>
+              </Col>
+              <Col md="3">
+                <div className="info">
+                  <div className="icon icon-info">
+                  <FontAwesomeIcon icon={faClock} />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">Proposal Tracking</h4>
+                    <p>
+                    Based on the consultancy model, track proposal changes from inception to Invoicing
+                    </p>
+                    {/* <Button className="btn-link" color="info" href="#pablo">
+                      See more
+                    </Button> */}
+                  </div>
+                </div>
+              </Col>
+              <Col md="3">
+                <div className="info">
+                  <div className="icon icon-info">
+                    <FontAwesomeIcon icon={faCogs} />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">Custom ERP</h4>
+                    <p>
+                      Create specific solutions for your business process to be integrated to your exsiting environment.
+                    </p>
+                    {/* <Button className="btn-link" color="info" href="#pablo">
+                      See more
+                    </Button> */}
+                  </div>
+                </div>
+              </Col>
+              <Col md="3">
+                <div className="info">
+                  <div className="icon icon-info">
+                    <FontAwesomeIcon icon={faChartLine} />
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">Dashboards</h4>
+                    <p>
+                      Create custom dashboards to better understand your companies performace
+                    </p>
+                    {/* <Button className="btn-link" color="info" href="#pablo">
+                      See more
+                    </Button> */}
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="section section-dark text-center">
+          <Container>
+            <h2 className="title">Let's Talk About Us</h2>
+            <Row>
+              <Col md="4">
+                {/* <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/clem-onojeghuo-3.jpg")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Mohammad Sultani</CardTitle>
+                        <h6 className="card-category">Lead Developer</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      Teamwork is so important that it is virtually impossible
+                      for you to reach the heights of your capabilities or make
+                      the money that you want without becoming very good at it.
+                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-google-plus" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card> */}
+              </Col>
+              <Col md="4">
+                <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/AviProfile.jpg")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Avinash Naidu</CardTitle>
+                        <h6 className="card-category">CEO</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      Creating a place for people to come together to learn, share and grow together.
+                      I want to get to the peak surrounded by the people that helped me along the way.
+                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    {/* <Button
+                      className="btn-just-icon btn-neutral"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-twitter" />
+                    </Button> */}
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="https://www.instagram.com/avinashofthewild/"
+                      target="_blank"
+                      //onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-instagram" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="https://www.linkedin.com/in/avinash-naidu-638ba977/"
+                      target="_blank"
+                      //onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+              <Col md="4">
+                {/* <Card className="card-profile card-plain">
+                  <div className="card-avatar">
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <img
+                        alt="..."
+                        src={require("assets/img/faces/erik-lucatero-2.jpg")}
+                      />
+                    </a>
+                  </div>
+                  <CardBody>
+                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                      <div className="author">
+                        <CardTitle tag="h4">Jack Te</CardTitle>
+                        <h6 className="card-category">Developer</h6>
+                      </div>
+                    </a>
+                    <p className="card-description text-center">
+                      The strength of the team is each individual member. The
+                      strength of each member is the team. If you can laugh
+                      together, you can work together, silence isn’t golden,
+                      it’s deadly.
+                    </p>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      className="btn-just-icon btn-neutral"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-google-plus" />
+                    </Button>
+                    <Button
+                      className="btn-just-icon btn-neutral ml-1"
+                      color="link"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      <i className="fa fa-linkedin" />
+                    </Button>
+                  </CardFooter>
+                </Card> */}
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="section landing-section">
+          <SubmitEmail />
+        </div>
+      </div>
+      
+    </>
+  );
+}
+
+export default LandingPage;
